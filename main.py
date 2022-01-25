35e8fcf5e4c496370f1fe92727ca779fbefb1b30
anish3343 <43268896+anish3343@users.noreply.github.com>
2022-01-25 18:58:06 -0500

    Created working bot

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged into {1} as {0.user}'.format(client, "XXXSERVER"))
+
+client.run(os.getenv('TOKEN'))
