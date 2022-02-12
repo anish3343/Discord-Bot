87ff3113a55c077b41784aa63d8c395daa7c2c68
anish3343 <43268896+anish3343@users.noreply.github.com>
2022-02-11 23:14:46 -0500

    added hello reply

--- main.py
+++ main.py
@@ -10,4 +10,12 @@ client = discord.Client()
 async def on_ready():
     print('Logged into {1} as {0.user}'.format(client, "XXXSERVER"))
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
 client.run(os.getenv('TOKEN'))
